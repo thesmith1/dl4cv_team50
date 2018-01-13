db1901c1edfe9a78dc7baa50183badd99f0795af
Giorgio <appfan3@gmail.com>
2018-01-13 19:08:30 +0100

    Preprocessing class done, inserted a demo script. Updated the structure of the repo. Updated the README.md

--- preprocessing/preproc.py
+++ preprocessing/preproc.py
@@ -1,1 +1,17 @@
-from pycocotools.coco import COCO
+'''
+Demo file for usage of the Preprocessor class
+Initialize the object with the required parameters, then
+call the process_images() method
+'''
+
+from preprocessor import Preprocessor
+
+data_dir = '../data/'
+annotations_dir = '../annotations/'
+train_annotations = '{}train2017.json'.format(annotations_dir)
+val_annotations = '{}val2017.json'.format(annotations_dir)
+
+preprocessor_train = Preprocessor(data_dir, train_annotations)
+preprocessor_train.process_images('../data2/')
+preprocessor_val = Preprocessor(data_dir, val_annotations)
+preprocessor_val.process_images('../data2/')
