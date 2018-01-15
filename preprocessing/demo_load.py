3430a4b8ac4987829f3ee7131411434223ebee14
Giorgio <appfan3@gmail.com>
2018-01-15 16:34:53 +0100

    Implemented json_loader.py, which creates the annotations file for the minimized dataset (sample images); bug fix on INaturalistDataset

--- preprocessing/demo_load.py
+++ preprocessing/demo_load.py
@@ -2,16 +2,81 @@
 Demo script for loading the dataset in batches
 '''
 
-from inaturalist import INaturalist
+from inaturalist_dataset import INaturalistDataset
 import matplotlib.pyplot as plt
+import torch
 
 data_dir = '../data/'
 annotations_dir = '../annotations/'
-train_annotations = '{}train2017.json'.format(annotations_dir)
+train_annotations = '{}train2017_min.json'.format(annotations_dir)
 val_annotations = '{}val2017.json'.format(annotations_dir)
 
-dataset = INaturalist(data_dir, train_annotations)
-print(dataset.__len__())
-index = 540719
-image, target = dataset.get_image(index)
-print(target)
+inaturalist = INaturalistDataset(data_dir, train_annotations)
+all_ids = inaturalist.ids
+images, targets = inaturalist.get_images(all_ids)
+#print(targets)
+#print(len(targets))
+
+batch_size = 10
+# train_loader = torch.utils.data.DataLoader(inaturalist, batch_size=batch_size, shuffle=True)
+
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+#         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+#         self.conv2_drop = nn.Dropout2d()
+#         self.fc1 = nn.Linear(320, 50)
+#         self.fc2 = nn.Linear(50, 10)
+
+#     def forward(self, x):
+#         x = F.relu(F.max_pool2d(self.conv1(x), 2))
+#         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+#         x = x.view(-1, 320)
+#         x = F.relu(self.fc1(x))
+#         x = F.dropout(x, training=self.training)
+#         x = self.fc2(x)
+#         return F.log_softmax(x)
+
+# model = Net()
+
+# optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+# def train(epoch):
+#     model.train()
+#     for batch_idx, (data, target) in enumerate(train_loader):
+#         if args.cuda:
+#             data, target = data.cuda(), target.cuda()
+#         data, target = Variable(data), Variable(target)
+#         optimizer.zero_grad()
+#         output = model(data)
+#         loss = F.nll_loss(output, target)
+#         loss.backward()
+#         optimizer.step()
+#         if batch_idx % args.log_interval == 0:
+#             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+#                 epoch, batch_idx * len(data), len(train_loader.dataset),
+#                 100. * batch_idx / len(train_loader), loss.data[0]))
+
+# def test():
+#     model.eval()
+#     test_loss = 0
+#     correct = 0
+#     for data, target in test_loader:
+#         if args.cuda:
+#             data, target = data.cuda(), target.cuda()
+#         data, target = Variable(data, volatile=True), Variable(target)
+#         output = model(data)
+#         test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+#         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+#         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+#     test_loss /= len(test_loader.dataset)
+#     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+#         test_loss, correct, len(test_loader.dataset),
+#         100. * correct / len(test_loader.dataset)))
+
+
+# for epoch in range(1, args.epochs + 1):
+#     train(epoch)
+#     test()
---
+++ preprocessing/json_loader.py
@@ -0,0 +1,37 @@
+import json
+
+train_file = json.load(open('../annotations/train2017.json'))
+val_file = json.load(open('../annotations/val2017.json'))
+print('loaded files')
+train_annotations = train_file['annotations']
+val_annotations = val_file['annotations']
+
+all_ids = [ann['image_id'] for ann in train_annotations if ann['category_id'] in [4985, 792, 4515]]
+all_ids.extend([ann['image_id'] for ann in val_annotations if ann['category_id'] in [4985, 792, 4515]])
+
+new_imgs_train = [img for img in train_file['images'] if img['id'] in all_ids]
+new_imgs_val = [img for img in val_file['images'] if img['id'] in all_ids]
+
+new_ann_train = [ann for ann in train_file['annotations'] if ann['image_id'] in all_ids]
+new_ann_val = [ann for ann in val_file['annotations'] if ann['image_id'] in all_ids]
+
+train_file['images'] = new_imgs_train
+val_file['images'] = new_imgs_val
+train_file['annotations'] = new_ann_train
+val_file['annotations'] = new_ann_val
+
+present_cat_ids_train = {ann['category_id'] for ann in train_file['annotations']}
+present_cat_ids_val = {ann['category_id'] for ann in val_file['annotations']}
+new_cat_train = [cat for cat in train_file['categories'] if cat['id'] in present_cat_ids_train]
+new_cat_val = [cat for cat in val_file['categories'] if cat['id'] in present_cat_ids_val]
+
+train_file['categories'] = new_cat_train
+val_file['categories'] = new_cat_val
+
+with open('../annotations/train2017_partial.json', 'w') as new_file:
+	json.dump(train_file, new_file)
+
+with open('../annotations/val2017_partial.json', 'w') as new_file:
+	json.dump(val_file, new_file)
+
+print("saved new files")
