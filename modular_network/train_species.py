485d48ecf4761d5ecefa98597a7c7768bcfbd1a3
Giorgio Fabbro <appfan3@gmail.com>
2018-01-25 10:13:03 +0100

    Preparing script train_species.py for training

--- modular_network/train_species.py
+++ modular_network/train_species.py
@@ -13,9 +13,11 @@ from torchvision import transforms
 
 lib_path = os.path.abspath(os.path.join(__file__, '../..'))
 sys.path.append(lib_path)
+ext_lib_path = os.path.abspath(os.path.join(__file__, '../../preprocessing'))
+sys.path.append(ext_lib_path)
 
-from preprocessing.inaturalist_dataset import INaturalistDataset
-from modular_network.modular_net import ModularNetwork
+from inaturalist_dataset import INaturalistDataset
+from modular_net import ModularNetwork
 
 cuda = torch.cuda.is_available()
 save = True
@@ -35,16 +37,16 @@ num_species = {'Actinopterygii': 53, 'Amphibia': 115, 'Animalia': 77, 'Arachnida
 
 data_dir = './data_preprocessed/'
 
-batch_size = 128
+batch_size = 800
 num_epochs = 1
-start_lr = 1000
+start_lr = 1e-3
 optimizers = ['sgd', 'adam', 'rmsprop']
 loss_functions = ['cross_entropy', 'l1', 'nll', 'l2']
 
 for cat in categories:
     annotations_dir = './annotations/modular_network/{}/'.format(cat)
-    train_annotations = '{}train2017_min.json'.format(annotations_dir)
-    val_annotations = '{}val2017_min.json'.format(annotations_dir)
+    train_annotations = '{}train2017_{}.json'.format(annotations_dir, cat)
+    val_annotations = '{}val2017_{}.json'.format(annotations_dir, cat)
 
     transf = transforms.ToTensor()
 
