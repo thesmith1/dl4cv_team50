2b0d2f5b283273b6557aa8bc66ab269598a8f86b
thesmith1 <appfan3@gmail.com>
2018-01-26 23:29:15 +0100

    train_core.py now appends new results to old ones when loading a model

--- modular_network/train_core.py
+++ modular_network/train_core.py
@@ -98,6 +98,8 @@ for optimizer in optimizers:
             print('Model loaded.')
 
         best_model, hist_acc, hist_loss = model.train('categories_net', num_epochs)
+        hist_acc = {'train': [], 'val': []}
+        hist_loss = {'train': [], 'val': []}
         if args.save:
             print('Saving best model...')
             model_filename = './modular_network/models/resnet50_{}_model_{}_{}.pth'.format('supercategories',
@@ -105,7 +107,16 @@ for optimizer in optimizers:
             torch.save(model, model_filename)
             print('Best model saved.')
             print('Saving results...')
-            results = {'accuracy': hist_acc, 'loss': hist_loss}
+            subfolders = args.model.split('.')[1].split('/')
+            old_results_filename = './' + subfolders[1] + '/results/' + subfolders[3] + '.pkl'
+            old_results = pickle.load(open(old_results_filename, 'rb'))
+            old_hist_acc = old_results['accuracy']
+            old_hist_loss = old_results['loss']
+            new_hist_acc = {'train': old_hist_acc['train'] + hist_acc['train'],
+                            'val': old_hist_acc['val'] + hist_acc['val']}
+            new_hist_loss = {'train': old_hist_loss['train'] + hist_loss['train'],
+                            'val': old_hist_loss['val'] + hist_loss['val']}
+            results = {'accuracy': new_hist_acc, 'loss': new_hist_loss}
             results_filename = './modular_network/results/resnet50_{}_results_{}_{}.pkl'.format('supercategories',
                                                                                                 optimizer, loss)
             with open(results_filename, 'wb') as output:
