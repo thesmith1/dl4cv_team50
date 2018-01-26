e35247c71f08332ef85d0ac5e1f4cc94d759c3c2
Paolo Notaro <s212323@studenti.polito.it>
2018-01-26 20:55:01 +0100

    added script to perform hyper-parameter tuning and try different models

---
+++ single_network/hyperparameter_tuning.py
@@ -0,0 +1,78 @@
+import sys
+import os
+from torchvision import models, transforms
+import torch
+import torch.optim as optim
+
+lib_path = os.path.abspath(os.path.join(__file__, '../..'))
+sys.path.append(lib_path)
+# print(sys.path)
+from single_network.train_complete_set import complete_train
+from preprocessing.inaturalist_dataset import INaturalistDataset
+
+# models
+architectures = [models.resnet50, models.inception_v3]
+model_input_sizes = {models.resnet50: 224, models.inception_v3: 299}
+
+# hyper-parameters
+learning_rates = [1e-4, 1e-3, 1e-2]
+regularization_strengths = [0, 1e-4, 1e-3, 1e-2, 1e-1]
+batch_size = 800
+num_epochs = 1
+optimizer = optim.Adam
+loss = torch.nn.CrossEntropyLoss
+applied_transformations = transforms.Compose([transforms.ToTensor()])
+
+# set directories
+data_dir = lambda size: './data_preprocessed_{}/'.format(size)
+annotations_dir = './annotations/'
+train_annotations = '{}train2017_new.json'.format(annotations_dir)
+val_annotations = '{}val2017.json'.format(annotations_dir)
+test_annotations = '{}test2017_new.json'.format(annotations_dir)
+
+# other parameters
+non_printable = ["model", "optimizer", "loss"]
+
+if __name__ == '__main__':
+
+    for model in architectures:
+
+        input_size = model_input_sizes[model]
+
+        # loading
+        print("Loading training set...")
+        inaturalist_train = INaturalistDataset(data_dir(input_size), train_annotations, transform=applied_transformations,
+                                               modular_network_remap=False)
+        train_loader = torch.utils.data.DataLoader(inaturalist_train, batch_size=batch_size, shuffle=True)
+        print("\n\nLoading validation set...")
+        inaturalist_val = INaturalistDataset(data_dir(input_size), val_annotations, transform=applied_transformations,
+                                             modular_network_remap=False)
+        val_loader = torch.utils.data.DataLoader(inaturalist_val, batch_size=batch_size, shuffle=True)
+
+        # print("\n\nLoading test set...")
+        # inaturalist_test = INaturalistDataset(data_dir, test_annotations, transform=applied_transformations,
+        # modular_network_remap=False)
+        # test_loader = torch.utils.data.DataLoader(inaturalist_test, batch_size, shuffle=True)
+        test_loader = None
+
+        for lr in learning_rates:
+            for reg in regularization_strengths:
+
+                # create parameter set
+                parameters = dict()
+                parameters['model'] = model
+                parameters['lr'] = lr
+                parameters['reg'] = reg
+                parameters['batch-size'] = batch_size
+                parameters['num-epochs'] = num_epochs
+                parameters['optimizer'] = optimizer
+                parameters['loss'] = loss
+                parameters['output-filename'] = "{0}.pth".format(
+                    "_".join([str(key) + "=" + (parameter.__name__ if key in non_printable else str(parameter))
+                              for key, parameter in parameters.items()]))
+
+                # train with combination of hyper-parameters
+                loaders = (train_loader, val_loader, test_loader)
+                print("Training model " + parameters['output-filename'])
+                complete_train(parameters, loaders)
+                print("done.")
