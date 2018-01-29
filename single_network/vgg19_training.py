7aa138aab5c6c0828f6a8ed94349e65c804e38cf
Paolo Notaro <s212323@studenti.polito.it>
2018-01-29 22:18:46 +0100

    added code from other script to overcome cuda error

--- single_network/vgg19_training.py
+++ single_network/vgg19_training.py
@@ -2,14 +2,13 @@ import sys
 import os
 from torchvision import models, transforms
 import torch
-import torch.nn as nn
 import torch.optim as optim
+from torch.autograd import Variable
 
 lib_path = os.path.abspath(os.path.join(__file__, '../..'))
 sys.path.append(lib_path)
 # print(sys.path)
 from preprocessing.inaturalist_dataset import INaturalistDataset
-import single_network.train_complete_set as train_script
 
 # paths
 data_dir = './data_preprocessed_224/'
@@ -31,7 +30,7 @@ do_testing = True
 applied_transformations = transforms.Compose([transforms.ToTensor()])
 non_printable = ["model", "optimizer", "loss"]
 cuda = torch.cuda.is_available()
-
+log_interval = 1
 
 def setup_vgg19(parameters, output_categories=667):
 
@@ -49,6 +48,123 @@ def setup_vgg19(parameters, output_categories=667):
     # create optimizer
     adam = optim.Adam(model.classifier.parameters(), lr=parameters['lr'], weight_decay=parameters['reg'])
     return model, adam
+
+
+def correct_predictions(output, target, topk=(1, 5)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).int().sum(0, keepdim=True)
+        res.append(correct_k.data.cpu().numpy().squeeze().tolist())
+    return res
+
+
+# single epoch of training method
+def one_epoch_train(model, optimizer, loss, train_loader, epoch):
+
+    # set train mode
+    model.train()
+
+    # for each batch
+    for batch_idx, (data, targets) in enumerate(train_loader):
+
+        # keep only species target
+        _, target = targets
+        # print("Batch dim:", data.shape)
+        data, (target) = Variable(data), Variable(target)
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        optimizer.zero_grad()
+
+        # forward pass
+        output = model(data)
+
+        # compute loss(es)
+        if isinstance(output, tuple):
+            loss_value = sum(loss(output_i, target) for output_i in output)
+        else:
+            loss_value = loss(output, target)
+
+        # backward pass
+        loss_value.backward()
+
+        # weight upgrade
+        optimizer.step()
+
+        # log
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.2f}%)] Loss: {:.6f}'.format(
+                epoch, batch_idx * train_loader.batch_size,
+                len(train_loader.dataset), 100. * batch_idx / len(train_loader),
+                loss_value.data[0]), end='\r')
+
+
+def evaluate(model, loss, dataset_loader):
+    # set evaluation mode
+    model.eval()
+
+    # initialization
+    loss_value = 0
+    top1_correct = 0
+    top5_correct = 0
+
+    # for each batch
+    for batch_idx, (data, targets) in enumerate(dataset_loader):
+
+        # keep only species target
+        _, target = targets
+
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+
+        # forward pass
+        output = model(data)
+
+        # compute loss
+        loss_value += loss(output, target).data[0]  # sum up batch loss
+
+        # predict
+        # top1_predicted_labels = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct1, correct2 = correct_predictions(output, target)
+
+        # update correct
+        top1_correct += correct1  # top1_predicted_labels.eq(target.data.view_as(top1_predicted_labels)).cpu().sum()
+        top5_correct += correct2
+
+        # log
+        if batch_idx % log_interval == 0:
+            print('Evaluated images: {}/{} ({:.2f}%)'.format(batch_idx * dataset_loader.batch_size,
+                                                             len(dataset_loader.dataset),
+                                                             100. * batch_idx / len(dataset_loader)), end='\r')
+
+    loss_value /= len(dataset_loader.dataset)
+
+    # final log
+    print('Evaluation completed.\nEvaluation results:\n'
+          'Average loss: {:.4f},\nTOP 1 Accuracy: {}/{} ({:.2f}%),\nTOP 5 Accuracy: {}/{} ({:.2f}%)\n'
+          .format(loss_value,
+                  top1_correct, len(dataset_loader.dataset), 100. * top1_correct / len(dataset_loader.dataset),
+                  top5_correct, len(dataset_loader.dataset), 100. * top5_correct / len(dataset_loader.dataset)))
+
+    return loss_value, top1_correct, top5_correct, len(dataset_loader.dataset)
+
+
+def save_model_statistics(output_filename, results_val, results_test=None):
+
+    with open(output_filename, "w") as fp:
+        print('Validation results\n'
+              'final loss: %.2f, correct (top1): %d, correct (top5): %d, predicted: %d\n\n' % results_val, file=fp)
+
+    if results_test is not None:
+        print("Test results\n"
+              "final loss: %.2f, correct (top1): %d, correct (top5): %d, predicted: %d\n\n" % results_test, file=fp)
 
 
 if __name__ == '__main__':
@@ -98,23 +214,23 @@ if __name__ == '__main__':
     # training
     print("Starting training (%d epoch%s)" % (num_epochs, "s" if num_epochs != 1 else ""))
     for epoch_count in range(1, num_epochs + 1):
-        train_script.one_epoch_train(model, adam, loss, train_loader, epoch_count)
+        one_epoch_train(model, adam, loss, train_loader, epoch_count)
         if validation_during_training:
-            train_script.evaluate(model, loss, val_loader)
+            evaluate(model, loss, val_loader)
 
     # evaluation on validation set
     print("\nEvaluating model on validation set...")
-    results_val = train_script.evaluate(model, loss, val_loader)
+    results_val = evaluate(model, loss, val_loader)
 
     # evaluation on test set
     results_test = None
     if test_loader is not None:
         print("Evaluating model on test set...")
-        results_test = train_script.evaluate(model, loss, test_loader)
+        results_test = evaluate(model, loss, test_loader)
 
     # saving
     model_output_file = "./single_network/models/" + parameters['output-filename']
     print("Saving model %s..." % model_output_file, end='')
     torch.save(model, model_output_file)
-    train_script.save_model_statistics(model_output_file[:-4] + ".txt", results_val, results_test)
+    save_model_statistics(model_output_file[:-4] + ".txt", results_val, results_test)
     print("done.")
